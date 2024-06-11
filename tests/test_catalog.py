346728e625f8d3e89a35af1e21cb89ce38b28389
Cano <cano@list.ru>
2024-06-12 00:57:39 +0300

    11-06 first test green

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -3,8 +3,7 @@ from settings import BASE_URL
 
 
 class TestCatalog:
-    def test_catalog(self, playwright_home_page: PlaywrightHomePage):
+    def test_open_catalog(self, playwright_home_page: PlaywrightHomePage):
         playwright_home_page.visit(BASE_URL)
         playwright_home_page.main_head_bar.open_catalog()
         playwright_home_page.catalog_bar.catalog_is_opened()
-gi
